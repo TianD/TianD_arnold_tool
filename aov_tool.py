a7e97f27fa6039a79b22c241e700c9d1dfc720a7
guoyu.hui <tiandao_dunjian@sina.cn>
2015-11-12 17:56:49 +0800

    2015/11/12
    bake updateDefaultFilterByName  in aov_tool

--- aov_tool.py
+++ aov_tool.py
@@ -17,8 +17,28 @@ reload(CNG)
 #定义每个AOV层的data类型
 DEFAULT_AOV_DATA_TYPES = {'AO': 'rgba', 'NOM': 'rgba', 'Fre': 'rgba', 'Zdp': 'rgba', 'sss': 'rgb', 'Z': 'float'}
 
+
+#编辑arnold的aov的默认filter字典
+def updateDefaultFilterByName(key, value = None):
+    if value:
+        aovs.defaultFiltersByName[key] = value
+    else :
+        if aovs.defaultFiltersByName.has_key(key):
+            del aovs.defaultFiltersByName[key]
+        else :
+            pass
+
+    return aovs.defaultFiltersByName
+
+
 def createAOV(name):
     aovInterface = aovs.AOVInterface()
+    
+    #
+    #默认的 Z通道 的AOV filter是closet, 项目需要改成默认的gaussian
+    #则将键 Z 从 arnold的aov默认filter字典里清除
+    if name == 'Z':
+        updateDefaultFilterByName('Z')
     
     #创建AOV
     if DEFAULT_AOV_DATA_TYPES.has_key(name):
